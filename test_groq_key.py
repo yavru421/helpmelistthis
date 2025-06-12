65bd698d4ea292e8376903bbcbd205a87c7b3ff0
JD.Dondlinger <dondlingergeneralcontracting@gmail.com>
2025-06-12 14:05:08 -0500

    Update test_groq_key.py

--- test_groq_key.py
+++ test_groq_key.py
@@ -1,6 +1,6 @@
 from groq import Groq
 
-api_key = "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"  # Paste your key here
+api_key = "your-api=key"  # Paste your key here
 client = Groq(api_key=api_key)
 try:
     models = client.models.list()
